d65c540f06f01e170861fdf71c0279c0553242c5
 Inna Meltsova <innameltsova@Innas-Air.lan>
2023-04-25 13:40:41 -0400

    ghhj

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post, Delete, Param, Body, Query } from '@nestjs/common';
 import { ChatService } from './chat.service';
-import { CreateChatDTO } from './create-chat.dto';
 
 @Controller('chat')
 export class ChatController {
@@ -14,11 +13,6 @@ export class ChatController {
     @Get(':chatId')
     async getChat(@Param('chatId') chatId: number) {
         return await this.chatService.getChat(chatId);
-    }
-
-    @Post()
-    async addChat(@Body() createChatDTO: CreateChatDTO) {
-        return await this.chatService.addChat(createChatDTO);
     }
 
     @Delete()
